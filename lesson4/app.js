080b707febd591c4909a08c26a72afcb73bad3bb
Andrii <gowar@ukr.net>
2023-07-30 20:30:44 +0300

    wokred

--- lesson4/app.js
+++ lesson4/app.js
@@ -28,35 +28,68 @@
 
 //TODO:=========task-02=================
 // Створіть контейнер div (з класом numberContainer )в HTML-документі та динамічно створіть 100 блоків (з класом number) наповнивши їх рандомними числами від 1 до 100 і додайте їх до контейнера div(numberContainer). Парні числа повинні мати зелений фон (додати клас even), Непарні числа - жовтий фон (додати клас odd).
+// function randomNumber() {
+//   return Math.floor(Math.random() * 100) + 1;
+// }
+
+// const containerDivEl = document.createElement("div");
+// containerDivEl.classList.add("number-container");
+// const blocks = 100;
+// const divArray = [];
+// for (let i = 0; i < blocks; i += 1) {
+//   const divEl = document.createElement("div");
+//   divEl.classList.add("number");
+//   divEl.textContent = randomNumber();
+//   if (divEl.textContent % 2 === 0) {
+//     divEl.classList.add("even");
+//   } else {
+//     divEl.classList.add("odd");
+//   }
+//   divArray.push(divEl);
+// }
+// containerDivEl.append(...divArray);
+// console.log(divArray);
+
+// const ulEl = document.querySelector(".list");
+// ulEl.after(containerDivEl);
 
 // //TODO:=========task-03=================
-// Створи HTML список фільмів на основі масиву під назвою films. Користувач обожнює дивитися кіно, дивиться його часто, тому при вході на сторінку він хоче бачити, які фільми вже були переглянуті, а до яких він ще не добрався. Вперу чергу зарендери фільми на сторінку, використовуй допоміжну функцію createMarkup(), яка буде повертати розмітку. Додай розмітку на сторінку задопомогою insertAdjacentHTML() і тільки після цього зроби елементи фільмів (li), які вже були переглянуті напівпрозорими (opacity: 0.5). Для цього використовуй масив унікальних id фільмів, знайди на сторінці елементи у яких id дорівнює тому id фільма, який користувач вже перелянув аби саме їх зробити напівпрозорими.
+// Створи HTML список фільмів на основі масиву під назвою films.
+//  Користувач обожнює дивитися кіно, дивиться його часто, тому при вході
+//  на сторінку він хоче бачити, які фільми вже були переглянуті, а до яких
+//   він ще не добрався.Вперу чергу зарендери фільми на сторінку, використовуй
+//    допоміжну функцію createMarkup(), яка буде повертати розмітку.Додай розмітку
+//     на сторінку задопомогою insertAdjacentHTML() і тільки після цього зроби
+//     елементи фільмів(li), які вже були переглянуті напівпрозорими(opacity: 0.5).
+//     Для цього використовуй масив унікальних id фільмів, знайди на сторінці
+//      елементи у яких id дорівнює тому id фільма, який користувач вже перелянув
+//       аби саме їх зробити напівпрозорими.
 
 const films = [
   {
-    title: 'Tetris',
-    imgUrl: 'https://static.hdrezka.ac/i/2023/3/20/f509264b419fdmu53x38j.jpg',
-    id: 'film_1',
+    title: "Tetris",
+    imgUrl: "https://static.hdrezka.ac/i/2023/3/20/f509264b419fdmu53x38j.jpg",
+    id: "film_1",
   },
   {
-    title: 'Avatar: The Way of Water',
-    imgUrl: 'https://static.hdrezka.ac/i/2022/12/22/tc5e6b8212683gn66r84s.jpg',
-    id: 'film_2',
+    title: "Avatar: The Way of Water",
+    imgUrl: "https://static.hdrezka.ac/i/2022/12/22/tc5e6b8212683gn66r84s.jpg",
+    id: "film_2",
   },
   {
-    title: 'Operation Fortune: Ruse de guerre',
-    imgUrl: 'https://static.hdrezka.ac/i/2022/2/11/s0d53f6cf0ae0tq29m85l.jpg',
-    id: 'film_3',
+    title: "Operation Fortune: Ruse de guerre",
+    imgUrl: "https://static.hdrezka.ac/i/2022/2/11/s0d53f6cf0ae0tq29m85l.jpg",
+    id: "film_3",
   },
   {
-    title: 'Babylon',
-    imgUrl: 'https://static.hdrezka.ac/i/2022/12/25/z330b47a82209ww99w55a.jpg',
-    id: 'film_4',
+    title: "Babylon",
+    imgUrl: "https://static.hdrezka.ac/i/2022/12/25/z330b47a82209ww99w55a.jpg",
+    id: "film_4",
   },
   {
-    title: 'The Whale',
-    imgUrl: 'https://static.hdrezka.ac/i/2023/2/24/h23d8c65d734akd89q94c.jpg',
-    id: 'film_5',
+    title: "The Whale",
+    imgUrl: "https://static.hdrezka.ac/i/2023/2/24/h23d8c65d734akd89q94c.jpg",
+    id: "film_5",
   },
 ];
 
@@ -68,12 +101,34 @@ const films = [
 </li> */
 }
 
-// const watchedFilms = ['film_2', 'film_4', 'film_5'];
+const watchedFilms = ["film_2", "film_4", "film_5"];
 
-// function createMarkup() {}
+function createMarkup(film) {
+  const { title, imgUrl, id } = film;
+  return `
+    <li id="${id}">
+      <img src="${imgUrl}" alt="${title}" />
+      <p>${title}</p>
+    </li>
+  `;
+}
+
+function renderFilms(arr) {
+  const ulEl = document.querySelector(".film-list");
+  const murkup = arr.map((el) => createMarkup(el)).join("");
+  ulEl.insertAdjacentHTML("beforeend", murkup);
+
+  watchedFilms.forEach((elem) => {
+    const film = document.querySelector(`#${elem}`);
+
+    if (film) {
+      film.style.opacity = 0.5;
+    }
+  });
+}
 
 // Виклик функції для рендерингу фільмів
-// renderFilms(films);
+renderFilms(films);
 
 //TODO:=========task-04=================
 /**
